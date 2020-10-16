3f8f7efc683ec27c062f4516662e7419571b3f66
Vlad Vrabie <vladvrabie97@gmail.com>
2020-10-16 16:11:15 +0200

    Added the windows service project

---
+++ MailingWindowsService/MailingWindowsService/MailService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailingWindowsService
+{
+    public partial class MailService : ServiceBase
+    {
+        public MailService()
+        {
+            InitializeComponent();
+
+            //ServiceName = "...";
+            //eventLogger.Source = "...";
+            //eventLogger.Log = "...";
+
+            if (!EventLog.SourceExists(eventLogger.Source))
+            {
+                EventLog.CreateEventSource(eventLogger.Source, eventLogger.Log);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLogger.WriteEntry("In OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            eventLogger.WriteEntry("In OnStop");
+        }
+    }
+}
---
+++ MailingWindowsService/MailingWindowsService/ProjectInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailingWindowsService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            //serviceInstaller1.ServiceName = "...";
+            //serviceInstaller1.DisplayName = "...";
+            //serviceInstaller1.Description = "...";
+        }
+    }
+}
