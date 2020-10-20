3649b71c16fa27ff24d8087c617c302593ed01ac
Vlad Vrabie <vladvrabie97@gmail.com>
2020-10-20 12:22:29 +0200

    Added email config reader to service project and updated app.config file

---
+++ MailingWindowsService/MailingWindowsService/EmailSender/AppConfigReader.cs
@@ -0,0 +1,41 @@
+﻿using MailingWindowsService.Logging;
+using System;
+using System.Configuration;
+using System.Linq;
+
+namespace MailingWindowsService.EmailSender
+{
+    class AppConfigReader
+    {
+        public ILogger logger;
+
+        private static readonly string SENDER_EMAIL = "senderEmail";
+        private static readonly string SENDER_PASSWORD = "senderEmailPassword";
+        private static readonly string RECEIVER_EMAILS = "receiverEmails";
+
+
+        public EmailSenderParameters GetEmailSenderParameters()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                if (appSettings.Count == 0)
+                {
+                    throw new ConfigurationErrorsException();
+                }
+
+                return new EmailSenderParameters()
+                {
+                    senderEmail = appSettings[SENDER_EMAIL],
+                    senderEmailPassword = appSettings[SENDER_PASSWORD],
+                    receiverEmails = appSettings[RECEIVER_EMAILS].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList(),
+                };
+            }
+            catch (Exception ex)
+            {
+                logger?.LogE($"Error in retrieving email sender parameters; Message: {ex.Message}");
+                return new EmailSenderParameters();
+            }
+        }
+    }
+}
